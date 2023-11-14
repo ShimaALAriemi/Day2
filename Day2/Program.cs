ec8fc093184d433c0cdf815c11ed13055ac8c87f
Shaima <shooomi9988@gmail.com>
2023-11-14 15:30:12 +0400

    first commit

---
+++ Day2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day2{
+    class Day2{ 
+        static void Main(String[]args)
+        {
+
+            String name = "Shaima Bakheet ";
+            String job = " Full stack web development";
+            int age = 24;
+            Console.WriteLine("My Name: " + name + " I'm worrking in:" +
+                job + " I'm :" + age);
+            Console.WriteLine("Hello Beautiful !");
+
+            Console.Write("Shima");
+        }
+    }
+}
